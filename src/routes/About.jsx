52cb5f3f3621e09b30dc5fe9f0f9f9bc3bd83224
Alessandro Sherhey <83360723+alessandro-sherhey@users.noreply.github.com>
2023-07-26 18:32:01 +0200

    Added temporary text for `About` route

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,4 +1,4 @@
-import { Typography, Divider } from "antd"
+import { Button, Typography, Divider, Row, Col } from "antd"
 import { Content } from "antd/es/layout/layout";
 import React from "react";
 
@@ -15,6 +15,8 @@ const About = () => {
         >
             <Typography.Title>About</Typography.Title>
             <Divider />
+            <Typography.Paragraph>This website was made by <Button type="link" style={{padding: 0}}>Alessandro Sherhey</Button>.</Typography.Paragraph>
+            <Typography.Paragraph>I used React, Redux for managing the app's state, React Router for client routing, React Markdown for the writer's live preview, Ant Design for the UI components and Ant Icons for the icons.</Typography.Paragraph>
         </Content>
 
     )
