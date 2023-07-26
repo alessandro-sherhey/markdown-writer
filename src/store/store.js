be5e1250378e35711985f8e9fa1fd692ff15c814
Alessandro Sherhey <83360723+alessandro-sherhey@users.noreply.github.com>
2023-07-26 21:39:52 +0200

    Removed `keysSlice`

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mdReducer from './MDSlice';
-import keysReducer from './KeysSlice';
 
 const store = configureStore({
     reducer: {
         md: mdReducer,
-        keys: keysReducer
     }
 })
 
