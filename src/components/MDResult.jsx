61f6e9d0731f1f219e7f01fc0f747590e4485835
Alessandro Sherhey <83360723+alessandro-sherhey@users.noreply.github.com>
2023-07-26 11:30:28 +0200

    Added `overflowY: auto` to MDResult

--- src/components/MDResult.jsx
+++ src/components/MDResult.jsx
@@ -6,14 +6,8 @@ import { useSelector } from "react-redux";
 const MDResult = () => {
     const text = useSelector(state => state.md)
     return (
-        <div className="mdresult" style={{height: 'calc(100vh - 190px)', border: '1px solid rgb(215, 215, 215)', borderRadius: 7, padding: '0px 13px'}}>
-            <ReactMarkdown
-                // components={{
-                //     h1: <Typography.Title level={1}></Typography.Title>,
-                //     h2: <Typography.Title level={2}></Typography.Title>,
-                //     h3: <Typography.Title level={3}></Typography.Title>
-                // }}
-            >
+        <div className="mdresult" style={{height: 'calc(100vh - 190px)', border: '1px solid rgb(215, 215, 215)', borderRadius: 7, padding: '0px 13px', overflowY: 'auto'}}>
+            <ReactMarkdown>
                 {text}
             </ReactMarkdown>
         </div>
