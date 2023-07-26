470eccab5719bdf9b7faf70b7321de0aa5087312
Alessandro Sherhey <83360723+alessandro-sherhey@users.noreply.github.com>
2023-07-26 21:58:51 +0200

    Updated keyboard shortcuts route

--- src/routes/Shortcuts.jsx
+++ src/routes/Shortcuts.jsx
@@ -17,7 +17,6 @@ const Shortcuts = () => {
         } else {
             setIsAppleDevice(false)
         }
-        console.log(isAppleDevice)
     })
 
     return (
@@ -39,14 +38,27 @@ const Shortcuts = () => {
                 <Col>
                     <b>Headings</b>
                 </Col>
-                <Col>
-                    <p>⌥ Option + 1</p>
-                    <p>⌥ Option + 2</p>
-                    <p>⌥ Option + 3</p>
-                    <p>⌥ Option + 4</p>
-                    <p>⌥ Option + 5</p>
-                    <p>⌥ Option + 6</p>
-                </Col>
+                {
+                    isAppleDevice
+                    ?
+                    <Col>
+                        <p>⌃ Control + 1</p>
+                        <p>⌃ Control + 2</p>
+                        <p>⌃ Control + 3</p>
+                        <p>⌃ Control + 4</p>
+                        <p>⌃ Control + 5</p>
+                        <p>⌃ Control + 6</p>
+                    </Col>
+                    :
+                    <Col>
+                        <p>Alt + 1</p>
+                        <p>Alt + 2</p>
+                        <p>Alt + 3</p>
+                        <p>Alt + 4</p>
+                        <p>Alt + 5</p>
+                        <p>Alt + 6</p>
+                    </Col>
+                }
             </Row>
             <Divider children="Text Styling"/>
             <Row align='middle' gutter={20}>
@@ -56,12 +68,17 @@ const Shortcuts = () => {
                 <Col>
                     <b>Bold</b>
                 </Col>
-                <Col>
-                    <p>⌘ Cmd + B</p>
-                </Col>
-                <Col>
-                    <p>⌥ Option + B</p>
-                </Col>
+                {
+                    isAppleDevice
+                    ?
+                    <Col>
+                        <p>⌘ Cmd + B</p>
+                    </Col>
+                    :
+                    <Col>
+                        <p>Ctrl + B</p>
+                    </Col>
+                }
             </Row>
             <Row align='middle' gutter={20}>
                 <Col>
@@ -70,12 +87,17 @@ const Shortcuts = () => {
                 <Col>
                     <b>Italics</b>
                 </Col>
-                <Col>
-                    <p>⌘ Cmd + I</p>
-                </Col>
-                <Col>
-                    <p>⌥ Option + I</p>
-                </Col>
+                {
+                    isAppleDevice
+                    ?
+                    <Col>
+                        <p>⌘ Cmd + I</p>
+                    </Col>
+                    :
+                    <Col>
+                        <p>Ctrl + I</p>
+                    </Col>
+                }
             </Row>
             <Row align='middle' gutter={20}>
                 <Col>
@@ -84,59 +106,69 @@ const Shortcuts = () => {
                 <Col>
                     <b>Strikethrough</b>
                 </Col>
-                <Col>
-                    <p>⌘ Cmd + S</p>
-                </Col>
-                <Col>
-                    <p>⌥ Option + S</p>
-                </Col>
+                {
+                    isAppleDevice
+                    ?
+                    <Col>
+                        <p>⌘ Cmd + S</p>
+                    </Col>
+                    :
+                    <Col>
+                        <p>Ctrl + S</p>
+                    </Col>
+                }
             </Row>
-            <Divider children="Text Formatting"/>
-            <Row align='middle' gutter={20}>
-                <Col>
-                    <Button icon={<DoubleRightOutlined />} />
-                </Col>
-                <Col>
-                    <b>Blockquote</b>
-                </Col>
-                <Col>
-                    <p>⌥ Option + Q</p>
-                </Col>
-            </Row>
-            <Row align='middle' gutter={20}>
-                <Col>
-                    <Button icon={<UnorderedListOutlined />} />
-                </Col>
-                <Col>
-                    <b>Unordered List</b>
-                </Col>
-                <Col>
-                    <p>⌥ Option + U</p>
-                </Col>
-            </Row>
-            <Row align='middle' gutter={20}>
-                <Col>
-                    <Button icon={<OrderedListOutlined />} />
-                </Col>
-                <Col>
-                    <b>Ordered List</b>
-                </Col>
-                <Col>
-                    <p>⌥ Option + O</p>
-                </Col>
-            </Row>
-            <Divider children="Other" />
-            <Row align='middle' gutter={20}>
-                <Col>
-                    <Button icon={<LinkOutlined />} />
-                </Col>
-                <Col>
-                    <b>Hyperlink</b>
-                </Col>
-                <Col>
-                    <p>⌥ Option + L</p>
-                </Col>
-            </Row>
+            {
+                !isAppleDevice &&
+                <div>
+                    <Divider children="Text Formatting"/>
+                    <Row align='middle' gutter={20}>
+                        <Col>
+                            <Button icon={<DoubleRightOutlined />} />
+                        </Col>
+                        <Col>
+                            <b>Blockquote</b>
+                        </Col>
+                        <Col>
+                            <p>Alt + Q</p>
+                        </Col>
+                    </Row>
+                    <Row align='middle' gutter={20}>
+                        <Col>
+                            <Button icon={<UnorderedListOutlined />} />
+                        </Col>
+                        <Col>
+                            <b>Unordered List</b>
+                        </Col>
+                        <Col>
+                            <p>Alt + U</p>
+                        </Col>
+                    </Row>
+                    <Row align='middle' gutter={20}>
+                        <Col>
+                            <Button icon={<OrderedListOutlined />} />
+                        </Col>
+                        <Col>
+                            <b>Ordered List</b>
+                        </Col>
+                        <Col>
+                            <p>Alt + O</p>
+                        </Col>
+                    </Row>
+                    <Divider children="Other" />
+                    <Row align='middle' gutter={20}>
+                        <Col>
+                            <Button icon={<LinkOutlined />} />
+                        </Col>
+                        <Col>
+                            <b>Hyperlink</b>
+                        </Col>
+                        <Col>
+                            <p>Alt + L</p>
+                        </Col>
+                    </Row>
+                </div>
+            }
         </Content>
     )
 }
